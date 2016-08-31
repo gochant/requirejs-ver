d4ae394be1b70cb42dcdee9112f17070931a8612
gochant <guochant@gmail.com>
2016-08-31 18:26:18 +0800

    modify logic

--- ver.js
+++ ver.js
@@ -2,13 +2,16 @@
 define(function () {
 
     function isFunction(x) {
-        return Object.prototype.toString.call(x) === '[object Function]';
+        return Object.prototype.toString.call(x) == '[object Function]';
     }
     function clone(obj) {
         if (obj === null || typeof (obj) !== 'object' || 'isActiveClone' in obj)
             return obj;
 
-        var temp = obj instanceof Date ? new obj.constructor() : obj.constructor(); //or new Date(obj);
+        if (obj instanceof Date)
+            var temp = new obj.constructor(); //or new Date(obj);
+        else
+            var temp = obj.constructor();
 
         for (var key in obj) {
             if (Object.prototype.hasOwnProperty.call(obj, key)) {
@@ -29,27 +32,29 @@ define(function () {
 
                 // RequireJS会返回 pkgs 里面的路径，可能会造成路径不是配置时路径
                 resourceName = resourceName.replace('/main', '');
-                app.widget.package(resourceName);
 
-                // 修复加载@路径失败的bug
-                var name = resourceName.split('@')[0];
-                require([name], function (templateContent) {
+                var resource = app.widget.normalizeConfig(resourceName);
+                var pkg = app.widget._getPackagesFrom(resource);
+                require.config({
+                    packages: [pkg]
+                });
+                require([pkg.name], function (templateContent) {
 
                     var obj = templateContent;
                     if (obj._widgetName) {
-                        obj._widgetName.push(name);
+                        obj._widgetName.push(resource.name);
                     } else {
-                        obj._widgetName = [name];
+                        obj._widgetName = [resource.name];
                     }
 
-                    if (!isFunction(templateContent)) {
+                    if (!isFunction(templateContent)) {                   
                         templateContent = app.view.define(obj);
                         templateContent.export = function () {
                             return clone(obj);
                         };
                     }
                     templateContent._widgetName = obj._widgetName;
-                    templateContent._source = app.widget.splitNameParts(resourceName).source;
+                    templateContent._source = resource.options._source;
                     callback(templateContent);
 
                 });
